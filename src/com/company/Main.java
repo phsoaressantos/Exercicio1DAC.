b795c66ea8f98349470da7c3f3c2655528c2c877
Paulo Henrique Santos <paulosantos@id.uff.br>
2021-11-06 15:52:00 -0300

    Teste Primeiro Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        //pegamos o ip da maquina.
+        String ipDaMaquina = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(ipDaMaquina);
+        //nome da maquina.
+        String nomeDaMaquina = InetAddress.getLocalHost().getHostName();
+        System.out.println(nomeDaMaquina);
+    }
+
+}
+
+
+
+
